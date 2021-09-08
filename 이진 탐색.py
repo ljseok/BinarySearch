abd30112286a466c1ce63bdb92087eb033daef64
ccccljs <cho38suk@hamail.net>
2021-09-08 14:29:43 +0900

    이진 탐색 파이썬

---
+++ 이진 탐색.py
@@ -0,0 +1,29 @@
+def binary_search(array, target, start, end): # 이진탐색 구현 함수
+    if start > end: # 시작점이 끝점보다 큰경우
+        return None
+    mid = (start + end) // 2
+
+    if array[mid] == target: # 찾은경우 중간점 인덱스 반환
+        return mid
+
+    elif array[mid] > target: # 중간 점 인덱스가 타깃보다 클 경우
+        return binary_search(array, target, start, mid-1) # 왼쪽 값 확인
+    else: # 중간 점 인덱스가 타깃보다 작을경우
+        return binary_search(array, target, mid+1, end) # 오른쪽 값 확인
+'''
+
+n, target = list(map(int,input().split()))
+array = list(map(int,input().split()))
+
+'''
+n = 10
+target = 7
+
+array = [1,3,5,7,9,11,13,15,17,19]
+
+result = binary_search(array, target, 0, n-1) # 이진탐색 결과 출력
+if result== None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result+1)
+
