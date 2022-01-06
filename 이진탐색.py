af023f3e7ecbf381039fd0db908dcf030e5fc5a2
ccccljs <cho38suk@hamail.net>
2022-01-06 10:55:35 +0900

    이진탐색 알고리즘

--- 이진탐색.py
+++ 이진탐색.py
@@ -1,29 +1,20 @@
-def binary_search(array, target, start, end): # 이진탐색 구현 함수
-    if start > end: # 시작점이 끝점보다 큰경우
+def binary_search(array, target, start, end):
+    if start > end: # 끝점보다 시작점이 크다면
         return None
-    mid = (start + end) // 2
+    mid = (start+end) // 2
 
-    if array[mid] == target: # 찾은경우 중간점 인덱스 반환
-        return mid
+    if array[mid] == target: # 찾았다면
+        return mid # 중간점 반횐
+    elif array[mid] > target: # 중간 점이 타깃보다 크다면
+        return binary_search(array, target, start, mid - 1) # 왼쪽확인
+    else:  # 중간 점이 타깃보다 작다면
+        return binary_search(array, target, mid + 1 , end) # 오"른쪽 확인
 
-    elif array[mid] > target: # 중간 점 인덱스가 타깃보다 클 경우
-        return binary_search(array, target, start, mid-1) # 왼쪽 값 확인
-    else: # 중간 점 인덱스가 타깃보다 작을경우
-        return binary_search(array, target, mid+1, end) # 오른쪽 값 확인
-'''
+n,target = list(map(int, input().split()))
+array = list(map(int, input().split()))
 
-n, target = list(map(int,input().split()))
-array = list(map(int,input().split()))
-
-'''
-n = 10
-target = 7
-
-array = [1,3,5,7,9,11,13,15,17,19]
-
-result = binary_search(array, target, 0, n-1) # 이진탐색 결과 출력
-if result== None:
-    print("원소가 존재하지 않습니다.")
+result = binary_search(array, target, 0, n-1)
+if result == None:
+    print("원소가 존재하지 않는다")
 else:
     print(result+1)
-
